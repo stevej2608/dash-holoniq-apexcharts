27fec1d7e7248dc73e19f4258e66dd0c5354d5e3
Steve Jones <jonesst2608@gmail.com>
2022-12-05 09:20:34 +0000

    App.js example now working

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,28 +1,96 @@
 /* eslint no-magic-numbers: 0 */
 import React, {Component} from 'react';
 
-import { DashHoloniqApexcharts} from '../lib';
+import { DashApexcharts} from '../lib';
 
 class App extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            value: ''
-        };
-        this.setProps = this.setProps.bind(this);
-    }
+          options:{
+            chart: {
+              fontFamily: 'Inter, sans-serif',
+              foreColor: '#6B7280',
+              toolbar: {
+                show: false
+              }
+            },
+            fill: {
+              type: 'solid',
+              opacity: 0.3,
+            },
+            dataLabels: {
+              enabled: false
+            },
+            tooltip: {
+              style: {
+                fontSize: '14px',
+                fontFamily: 'Inter, sans-serif',
+              },
+            },
+            grid: {
+              show: false,
+            },
+            xaxis: {
+              categories: ['01 Feb', '02 Feb', '03 Feb', '04 Feb', '05 Feb', '06 Feb', '07 Feb'],
+              labels: {
+                style: {
+                  colors: ['#6B7280'],
+                  fontSize: '14px',
+                  fontWeight: 500,
+                },
+              },
+              axisBorder: {
+                color: '#F3F4F6',
+              },
+              axisTicks: {
+                color: '#F3F4F6',
+              }
+            },
+            yaxis: {
+              labels: {
+                style: {
+                  colors: ['#6B7280'],
+                  fontSize: '14px',
+                  fontWeight: 500,
+                },
+                formatter: function (value) {
+                  return '$' + value;
+                }
+              },
+            },
+            responsive: [
+              {
+                breakpoint: 1024,
+                options: {
+                  xaxis: {
+                    labels: {
+                      show: false
+                    }
+                  }
+                }
+              }
+            ]
+          },
+          series: [
+            {
+              name: 'Revenue',
+              data: [6356, 6218, 6156, 6526, 6356, 6256, 6056],
+              color: '#0694a2'
+            },
+          ]
 
-    setProps(newProps) {
-        this.setState(newProps);
+        };
     }
 
     render() {
         return (
             <div>
-                <DashHoloniqApexcharts
-                    setProps={this.setProps}
-                    {...this.state}
+              <DashApexcharts options={this.state.options}
+                        series={this.state.series}
+                        type="area"
+                        width="420"
                 />
             </div>
         )
